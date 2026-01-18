3a99571bdaa17eb9a7f499eb2c673da2e39047f6
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2026-01-18 15:46:29 +0100

    Return a list of Books: "/api/books" Endpoint

--- books/src/main/java/com/dgmf/BooksApplication.java
+++ books/src/main/java/com/dgmf/BooksApplication.java
@@ -1,7 +1,11 @@
 package com.dgmf;
 
+import com.dgmf.entity.Book;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class BooksApplication {
--- books/src/main/java/com/dgmf/controller/BookController.java
+++ books/src/main/java/com/dgmf/controller/BookController.java
@@ -1,12 +1,39 @@
 package com.dgmf.controller;
 
+import com.dgmf.entity.Book;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class BookController {
-    @GetMapping("/api")
+    private final List<Book> books = new ArrayList<>();
+
+    public BookController() {
+        initializeBooks();
+    }
+
+    private void initializeBooks() {
+        books.addAll(List.of(
+                new Book("Title one", "Author one", "Science"),
+                new Book("Title two", "Author two", "Science"),
+                new Book("Title three", "Author three", "History"),
+                new Book("Title four", "Author four", "Maths"),
+                new Book("Title five", "Author five", "Maths"),
+                new Book("Title six", "Author six", "Maths")
+        ));
+    }
+
+    /*@GetMapping("/api")
     public String firstAPI() {
         return "Hello Glaz !";
+    }*/
+
+//    @GetMapping
+    @GetMapping("/api/books")
+    public List<Book> getBooks() {
+        return books;
     }
 }
